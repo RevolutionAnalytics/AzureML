f6d9e4fcd133cd390f566aca85aa10aa555260a2
Antonio Piccolboni <antonio@piccolboni.info>
2015-08-04 11:45:34 -0700

    added names to experiments and datasets, both as names function and in as.list #5

--- pkg/R/generic.R
+++ pkg/R/generic.R
@@ -2,6 +2,19 @@
 
 as.data.frame.Dataset = function(x) x$as.data.frame()
 `[[.Datasets` = function(xx, i) xx$get.item(i)
-as.list.Datasets = function(xx) xx$get.datasets()
+
+as.list.Datasets =
+  function(xx) {
+    yy = xx$get.datasets()
+    setNames(yy, unlist(map(as.list(yy), "Name")))}
+
+names.Datasets = function(xx) names(as.list(xx))
+
 `[[.Experiments` = function(xx, i) xx$get.item(i)
-as.list.Experiments = function(xx) xx$get.experiments()
+
+as.list.Experiments =
+  function(xx) {
+    yy = xx$get.experiments()
+    setNames(yy, unlist(map(as.list(yy), "ExperimentId")))}
+
+names.Experiments = function(xx) names(as.list(xx))
