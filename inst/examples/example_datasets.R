26f81fa92c8d5f1c38370851209f4f9009199afe
Andrie de Vries <apdevries@gmail.com>
2015-10-22 12:13:15 +0100

    Wrap examples in \dontrun #16

--- inst/examples/example_datasets.R
+++ inst/examples/example_datasets.R
@@ -1,21 +1,23 @@
-keyfile <- system.file("tests/testthat/config.json", package = "azureml")
-jsondata <- jsonlite::fromJSON(keyfile)
-list2env(jsondata, envir = globalenv())
-
-library(azureml)
-
-# workspace_id <- ""
-# authorization_token <- ""
-dataset <- "New York weather"
-
-ws <- workspace(
-  id = workspace_id,
-  auth = authorization_token
-)
-
-ws$datasets
-datasets(ws)
-
-ds <- match(dataset, ws$datasets$Name)
-frame <- download.datasets(ws$datasets[ds, ])
-head(frame)
+\dontrun{
+  keyfile <- system.file("tests/testthat/config.json", package = "azureml")
+  jsondata <- jsonlite::fromJSON(keyfile)
+  list2env(jsondata, envir = globalenv())
+  
+  library(azureml)
+  
+  # workspace_id <- ""
+  # authorization_token <- ""
+  dataset <- "New York weather"
+  
+  ws <- workspace(
+    id = workspace_id,
+    auth = authorization_token
+  )
+  
+  ws$datasets
+  datasets(ws)
+  
+  ds <- match(dataset, ws$datasets$Name)
+  frame <- download.datasets(ws$datasets[ds, ])
+  head(frame)
+}
