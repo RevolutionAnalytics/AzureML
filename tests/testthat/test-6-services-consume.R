c9e59fa7691eec78545c406b9f6bcbce0abeaf95
Andrie de Vries <apdevries@gmail.com>
2015-11-02 12:08:51 +0000

    Enabled tests (although not passing) #26

--- tests/testthat/test-6-services-consume.R
+++ tests/testthat/test-6-services-consume.R
@@ -2,7 +2,7 @@ context("Consume API")
 
 schemaUrl <- "https://studio.azureml.net/apihelp/workspaces/f5e8e9bc4eed4034b78567449cfca779/webservices/d42667a354e34a3f98888ba86300fc2f/endpoints/b4caf0f0ebfd451bbc187741894e213b/score"
 
-capture.output(schema <- discoverSchema(schemaUrl))
+schema <- discoverSchema(schemaUrl)
 
 schema
 schema$sampleInput$Gender <- "male"
