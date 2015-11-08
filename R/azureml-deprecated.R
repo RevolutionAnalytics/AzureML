567c564cabae49ef3328c76d78ff7a9a04a95135
Andrie de Vries <apdevries@gmail.com>
2015-11-07 17:43:01 -0800

    List deprecated functions. #35

---
+++ R/azureml-deprecated.R
@@ -0,0 +1,16 @@
+#' Deprecated functions
+#' 
+#' @rdname AzureML-deprecated
+consumeDataframe <- function(){
+  .Deprecated("consume")
+}
+
+#' @rdname AzureML-deprecated
+consumeFile <- function(){
+  .Deprecated("consume")
+}
+
+#' @rdname AzureML-deprecated
+consumeLists <- function(){
+  .Deprecated("consume")
+}
