0428033535ac24c4a0cbeb0d7a7a33c2f09c2a64
Andrie de Vries <apdevries@gmail.com>
2015-11-07 16:22:01 -0800

    Minor changes. #34

--- tests/testthat/test-2-datasets-upload-download-delete.R
+++ tests/testthat/test-2-datasets-upload-download-delete.R
@@ -1,10 +1,12 @@
 if(interactive()) library("testthat")
 
-if(file.exists("~/.azureml/settings.json"))
+settingsFile <- "~/.azureml/settings.json" 
+if(file.exists(settingsFile))
 {
   context("Upload and delete dataset")
   
-  timestamped_name <- paste0("dataset-test-upload-", format(Sys.time(), format="%Y-%m-%d--%H-%M-%S"))
+  timestamped_name <- paste0("dataset-test-upload-", 
+                             format(Sys.time(), format="%Y-%m-%d--%H-%M-%S"))
   
   test_that("Can upload dataset to workspace", {
     ws <- workspace()
@@ -12,14 +14,18 @@ if(file.exists("~/.azureml/settings.json"))
     ds <- datasets(ws, filter = "my")
     expect_true(timestamped_name %in% ds$Name)
   })
-
+  
   test_that("Can download dataset", {
-    expect_true(isTRUE(all.equal(download.datasets(ws, name=timestamped_name), airquality)))
+    ws <- workspace()
+    dl <- download.datasets(ws, name=timestamped_name)
+    expect_equal(dl, airquality)
   })
-
+  
   test_that("Can delete dataset from workspace", {
     ws <- workspace()
-    delete.datasets(ws, timestamped_name)
+    z <- delete.datasets(ws, timestamped_name)
+    expect_true(timestamped_name %in% z$Name)
+    # refresh(ws)
     ds <- datasets(ws, filter = "my")
     expect_false(timestamped_name %in% ds$Name)
   })
