e586306cd83e1feb75d24055003662af13345211
Andrie de Vries <apdevries@gmail.com>
2015-11-08 09:19:01 -0800

    Code formatting for readability

--- R/discover.R
+++ R/discover.R
@@ -27,11 +27,10 @@
 #' The result is cached in the workspace environment similarly to datasets and experiments.
 #'
 #' @inheritParams refresh
-#' @param service_id optional web service id. If supplied, return the web service information for just
-#' the specified service id. Leave undefined to return a data.frame of all services.
-#' @param name optional web service name. If supplied, return the web service information for services
-#' with matching names. Leave undefined to return all services.
+#' @param service_id optional web service id. If supplied, return the web service information for just the specified service id. Leave undefined to return a data.frame of all services.
+#' @param name optional web service name. If supplied, return the web service information for services with matching names. Leave undefined to return all services.
 #' @param uri the AzureML web services URI
+#' 
 #' @return Returns a data.frame with variables:
 #' \itemize{
 #'   \item Id
@@ -47,7 +46,7 @@
 #' @examples
 #' \dontrun{
 #' workspace_id <- ""          # Your AzureML workspace id
-#' authorization_token <- ""   # Your AsureML authorization token
+#' authorization_token <- ""   # Your AzureML authorization token
 #'
 #' ws <- workspace(
 #'   id = workspace_id,
@@ -70,18 +69,23 @@ services = function(ws, service_id, name, uri="https://management-tm.azureml.net
   handle_setheaders(h, .list=headers)
   if(missing(service_id)) service_id = ""
   else service_id = sprintf("/%s", service_id)
-  r = curl(sprintf("%s/workspaces/%s/webservices%s", uri, ws$id, service_id), handle=h)
+  r = curl(
+    sprintf("%s/workspaces/%s/webservices%s", uri, ws$id, service_id), 
+    handle=h
+    )
   on.exit(close(r))
   ans = tryCatch(fromJSON(readLines(r, warn=FALSE)), error=function(e) NULL)
   # Cache the result in the workspace
-  if(service_id=="") ws$services = ans
-  if(!missing(name)) return(ans[ans$Name==name,])
+  if(service_id == "") ws$services = ans
+  if(!missing(name)) return(ans[ans$Name == name,])
   ans
 }
+
 
 #' @rdname services
 #' @export
 getWebServices = services
+
 
 #' List AzureML Web Service Endpoints
 #'
@@ -90,8 +94,8 @@ getWebServices = services
 #' @inheritParams refresh
 #' @param uri The AzureML web services URI
 #' @param service_id A web service Id, for example returned by \code{\link{services}}.
-#' @param endpoint_id An optional endpoint id. If supplied, return the endpoint information for just
-#' that id. Leave undefined to return a data.frame of all end points associated with the service.
+#' @param endpoint_id An optional endpoint id. If supplied, return the endpoint information for just that id. Leave undefined to return a data.frame of all end points associated with the service.
+#' 
 #' @return Returns a data.frame with variables:
 #' \itemize{
 #'  \item Name
@@ -144,13 +148,22 @@ endpoints = function(ws, service_id, endpoint_id, uri="https://management-tm.azu
   if(missing(endpoint_id)) endpoint_id = ""
   else endpoint_id = sprintf("/%s", endpoint_id)
   if(is.list(service_id)) service_id = service_id$Id[1]
-  r = curl(sprintf("%s/workspaces/%s/webservices/%s/endpoints%s", uri, ws$id, service_id, endpoint_id), handle=h)
+  r = curl(
+    sprintf("%s/workspaces/%s/webservices/%s/endpoints%s", 
+            uri, 
+            ws$id, 
+            service_id, 
+            endpoint_id
+    ), 
+    handle=h)
   on.exit(close(r))
   ans = fromJSON(readLines(r, warn=FALSE))
   # Adjust the returned API location for completeness:
   if(length(ans)>0)
   {
-    ans$ApiLocation = paste(ans$ApiLocation, "/execute?api-version=2.0&details=true&format=swagger", sep="")
+    ans$ApiLocation = paste(ans$ApiLocation, 
+                            "/execute?api-version=2.0&details=true&format=swagger", 
+                            sep="")
   }
   ans
 }
@@ -167,6 +180,7 @@ getEndpoints = endpoints
 #'
 #' @param e an AzureML web service endpoint from the \code{\link{endpoints}} function.
 #' @param type the type of help to display.
+#' 
 #' @return The help text is invisibly returned.
 #' @family discovery functions
 #' @examples
@@ -185,7 +199,7 @@ getEndpoints = endpoints
 #' 
 #' }
 #' @export
-endpointHelp = function(e, type=c("r-snippet","score","jobs","update"))
+endpointHelp = function(e, type = c("r-snippet","score","jobs","update"))
 {
   type = match.arg(type)
   rsnip = FALSE
@@ -195,13 +209,24 @@ endpointHelp = function(e, type=c("r-snippet","score","jobs","update"))
     rsnip = TRUE
   }
   pattern = "</?\\w+((\\s+\\w+(\\s*=\\s*(?:\".*?\"|'.*?'|[^'\">\\s]+))?)+\\s*|\\s*)/?>"
-  text = paste(gsub("&.?quot;","'",gsub(pattern,"\\1",readLines(curl(paste(e$HelpLocation[1], type, sep="/")), warn=FALSE))), collapse="\n")
+  text = paste(
+    gsub(
+      "&.?quot;", "'", 
+      gsub(pattern, "\\1", 
+           readLines(
+             curl(paste(e$HelpLocation[1], type, sep="/")), warn = FALSE)
+      )
+    ), 
+    collapse="\n"
+  )
   if(rsnip)
   {
-    text = substr(text,grepRaw("code-snippet-r",text)+nchar("code-snippet-r")+2,nchar(text))
+    text = substr(text, 
+                  grepRaw("code-snippet-r",text)+nchar("code-snippet-r")+2,nchar(text)
+    )
   }
   f = tempfile()
-  writeLines(text, con=f)
+  writeLines(text, con = f)
   file.show(f)
   unlink(f)
   invisible(text)
