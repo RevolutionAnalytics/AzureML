82caf3534e65e85147237da70734dd6b23df6da1
Andrie de Vries <apdevries@gmail.com>
2015-11-13 13:36:11 -0800

    Test all dataset types #24

---
+++ tests/testthat/test-4-download-each-dataset-type.R
@@ -0,0 +1,37 @@
+if(interactive()) library("testthat")
+
+settingsFile <- "~/.azureml/settings.json" 
+if(file.exists(settingsFile))
+{
+  context("Download one file of every type")
+
+  ws <- workspace()
+  ds <- datasets(ws, filter = "samples")
+  unique(ds$DataTypeId)
+  
+  oneOfEach <- do.call(
+    rbind, 
+    lapply(
+      split(ds, ds$DataTypeId),
+      function(x){
+        x[which.min(x$Size), ]
+      }
+    )
+  )
+  
+  oneOfEach <- oneOfEach[!oneOfEach$DataTypeId %in% c("Zip"), ]
+  oneOfEach$DataTypeId
+  
+  for(type in oneOfEach$DataTypeId){
+  test_that(sprintf("Can download dataset of type %s", type), {
+    dl <- download.datasets(ws, name = oneOfEach$Name[oneOfEach$DataTypeId == type])
+    expect_is(dl, "data.frame")
+    expect_true(nrow(dl) > 0)
+  })
+  }
+  
+} else
+{
+  message("To run tests, add a file ~/.azureml/settings.json containing AzureML keys, see ?workspace for help")
+  message("No tests ran")
+}
