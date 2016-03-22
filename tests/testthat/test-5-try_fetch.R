18275f364b45b0b3f2449146d96abfcb1dde6274
Andrie de Vries <apdevries@gmail.com>
2016-03-22 04:02:44 +0000

    Update test #96

--- tests/testthat/test-5-try_fetch.R
+++ tests/testthat/test-5-try_fetch.R
@@ -10,7 +10,7 @@ test_that("try_fetch() gives exponential retry messages",{
       status_code <- if(runif(1) > 0.26) sample(retry_on, 1) else 200
       list(status_code = status_code, contents = NA)
     }, {
-      msg <- "Request failed with status 440. Waiting 0.1 seconds before retry\n"
+      msg <- "Request failed with status 509. Waiting 0.0 seconds before retry\n"
       expect_message(
         try_fetch(delay = 0.1, no_message_threshold = 0),
         msg
